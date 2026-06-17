7c05da39ebd2165f053c10f155edbd18dc2af14e
ganeshcoder05 <vermaganesh753>
2026-06-17 23:21:28 +0530

    fix netlify url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: ['http://localhost:3000',
-    'iridescent-axolotl-6513a4.netlify.app'
+    'https://iridescent-axolotl-6513a4.netlify.app'
   ],
   credentials: true
 }));
